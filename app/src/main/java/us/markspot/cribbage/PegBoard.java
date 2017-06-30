01acfaafaf042896d5844f11b6016c3a8b6eb846
markendsley <mr.markendsley@gmail.com>
2017-06-30 09:34:56 -0400

    Update Commit

---
+++ app/src/main/java/us/markspot/cribbage/PegBoard.java
@@ -0,0 +1,11 @@
+package us.markspot.cribbage;
+
+/**
+ * Created by Bob Dole on 6/30/2017.
+ */
+
+public class PegBoard {
+
+    public int playerOneLocation = 0;
+    public int playerTwoLocation = 0;
+}

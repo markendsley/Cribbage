fb291a06bc7d05939e2a463fe00ec1583f4a508e
markendsley <mr.markendsley@gmail.com>
2017-06-30 09:17:55 -0400

    Update Commit

--- app/src/main/java/us/markspot/cribbage/Card.java
+++ app/src/main/java/us/markspot/cribbage/Card.java
@@ -1,41 +1,55 @@
 package us.markspot.cribbage;
 
 /**
- * Created by Bob Dole on 6/29/2017.
+ * Created by Mark Endsley on 6/29/2017.
  */
 
-public abstract class Card {
+public class Card {
 
     public int value;
     public boolean outOfDeck = false;
     public boolean inPlay = false;
+    public boolean isKing = false;
+    public boolean isQueen = false;
+    public boolean isJack = false;
 
-    public enum suit{
+    public enum Suit{
         HEART, SPADE, CLUB, DIAMOND;
-
-        suit suitCode;
-
-        private suit(suit suitCode){
-            this.suitCode = suitCode;
-        }
-
     }
 
-    public Card(int value){
+    Suit suit;
+
+    public Card(int value, Suit suit){
         this.value = value;
+        this.suit = suit;
     }
 
-    public draw(){
+    public Card(int value, Suit suit, int royalty){
+        this.value = value;
+        this.suit = suit;
+        if(royalty == 0)
+            this.isJack = true;
+        else if(royalty == 1)
+            this.isQueen = true;
+        else if(royalty == 2)
+            this.isKing = true;
+
+    }
+
+    public void draw(){
         this.outOfDeck = true;
     }
     public boolean isOutOfDeck()
     {
         return this.outOfDeck;
     }
-    public play(){
+    public void play(){
         this.inPlay = true;
     }
     public boolean isInPlay(){
         return this.inPlay;
+    }
+    public Suit getSuit(){
+        return suit;
     }
 }
